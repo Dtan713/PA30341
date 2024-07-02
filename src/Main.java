04cf809fa29e2f8ebde38dd50ff148a5cc5871cf
Dtan713 <s.donaldtan@gmail.com>
2024-07-02 11:07:08 -0400

    first PA

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        // Declare an integer variable x and assign 7 to it
+        int x = 8;
+
+        // Write an if statement to print "Less than 10" if x is less than 10
+        if (x < 10) {
+            System.out.println("Less than 10");
+        }
+
+        // Change x to equal 15
+        x = 15;
+
+        // Notice that the console does not display anything because x is not less than 10 anymore
+    }
+}
